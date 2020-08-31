ad9a58ab2f6d7277b0895baae8921c007513e20c
Stefan Peters <Stefan.Peters@gbv.de>
2020-08-31 14:26:44 +0200

    Add file with license information to build output

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,6 +1,25 @@
+const path = require("path")
+const license = require("rollup-plugin-license")
+
 module.exports = {
   optimizeDeps: {
     include: ["codemirror"],
   },
   esbuildTarget: "es2015",
+  rollupInputOptions: {
+    plugins: [
+      // Add a plugin that assembles licenses for dependencies
+      license({
+        banner: {
+          commentStyle: "ignored",
+          content: "For license information, please see LICENSES.txt.",
+        },
+        thirdParty: {
+          output: {
+            file: path.join(__dirname, "dist", "_assets", "LICENSES.txt"),
+          },
+        },
+      }),
+    ],
+  },
 }
