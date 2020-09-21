986d793174fe95023810859c6314465202c5b0d2
Jakob Voss <voss@gbv.de>
2020-09-21 08:21:10 +0200

    Add test

--- test/pica-jskos.js
+++ test/pica-jskos.js
@@ -5,8 +5,10 @@ import { picaSchemes } from "../src/lib/pica-jskos.js"
 import { PicaPath } from "pica-data"
 
 describe("picaSchemes", () => {
-  const schemes = picaSchemes([{ uri: "http://example.org", PICAPATH: "000X$x" }])
-  assert.deepEqual(Object.keys(schemes),["http://example.org"])
-  assert(schemes["http://example.org"] instanceof ConceptScheme)
-  assert(schemes["http://example.org"].PICAPATH instanceof PicaPath)
+  it("constructs a schemes object", () => {
+    const schemes = picaSchemes([{ uri: "http://example.org", PICAPATH: "000X$x" }])
+    assert.deepEqual(Object.keys(schemes),["http://example.org"])
+    assert(schemes["http://example.org"] instanceof ConceptScheme)
+   assert(schemes["http://example.org"].PICAPATH instanceof PicaPath)
+  })
 })
