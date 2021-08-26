0b13f3d34efaad7d60778835cdc6f6ad362e161f
Jakob Voss <voss@gbv.de>
2021-08-26 15:46:04 +0200

    rvk2bk: extend script

--- rvk2bk/rvkbk-mappings.sh
+++ rvk2bk/rvkbk-mappings.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# Get mappings of type < or = from a given RVK notation to BK, including annotations
+# Get mappings of type < or = from any or a given RVK notation to BK, including annotations
+RVK=$1
+LIMIT=2000
 
 curl -s -G "http://coli-conc.gbv.de/api/mappings" \
-  --data-urlencode "from=$1" \
+  `if [ "$RVK" ]; then echo "--data-urlencode \"from=$RVK\""; fi` \
   --data-urlencode "fromScheme=http://uri.gbv.de/terminology/rvk/" \
   --data-urlencode "toScheme=http://uri.gbv.de/terminology/bk/" \
   --data-urlencode "properties=annotations" \
+  --data-urlencode "limit=$LIMIT" \
   --data-urlencode "type=http://www.w3.org/2004/02/skos/core#exactMatch|http://www.w3.org/2004/02/skos/core#narrowMatch" \
-  | jq -c .[]
+| jq -c .[]
