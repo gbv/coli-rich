79999113e716bdb8d12c439b875e70c560288f42
Stefan Peters <Stefan.Peters@gbv.de>
2024-03-07 12:06:18 +0100

    Add workaround for running behind nginx on subpath

--- vue/main.js
+++ vue/main.js
@@ -5,9 +5,17 @@ const app = createApp(App)
 
 // Load and configure vue-router
 import { createRouter, createWebHistory } from "vue-router"
-app.use(createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes: [ { path: "/:catchAll(.*)", component: App } ],
-}))
+})
+app.use(router)
+router.afterEach((to) => {
+  // Really dirty workaround for a router issue where slashes get replaced by %2F
+  if (to.fullPath.includes("%2F")) {
+    const newPath = to.fullPath.replaceAll("%2F", "/")
+    history.replaceState({}, null, newPath)
+  }
+})
 
 app.mount("#app")
